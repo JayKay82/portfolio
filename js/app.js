757a6bc2bed6c31e85eb0b0d6521c9d57066471b
Jan Kroezen <jankroezen@me.com>
2016-09-15 15:25:42 +0100

    add first mobile breakpoint at 480px that modifies the size and/or positioning of the title, burger and close icons, social media icons and navigation links

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+// Opens the Navigation Menu
 $("header > span.fa").on("click", function(e){
   e.preventDefault();
   console.log("Menu opened");
   $('#mobile-nav').addClass('visible');
 });
 
+//  Closes the Navigation Menu
 $("#mobile-nav > span.fa").on("click", function(e){
   e.preventDefault();
   console.log("Menu closed");
