551cb9d95b4bea95a077e688123b56de91803a46
Jan Kroezen <jankroezen@me.com>
2016-09-19 21:06:22 +0100

    Add a profile section for mobile to the about page.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
-// Opens the Navigation Menu
-$("header > span.fa").on("click", function(e){
-  e.preventDefault();
-  console.log("Menu opened");
-  $('#mobile-nav').addClass('visible');
-});
+'use strict';
 
-//  Closes the Navigation Menu
-$("#mobile-nav > span.fa").on("click", function(e){
-  e.preventDefault();
-  console.log("Menu closed");
-  $('#mobile-nav').removeClass('visible');
+$(document).ready(function(){
+
+  // Opens the Navigation Menu
+  $("header > span.fa").on("click", function(e){
+    e.preventDefault();
+    console.log("Menu opened");
+    $('#mobile-nav').addClass('visible');
+  });
+
+  //  Closes the Navigation Menu
+  $("#mobile-nav > span.fa").on("click", function(e){
+    e.preventDefault();
+    console.log("Menu closed");
+    $('#mobile-nav').removeClass('visible');
+  });
+
+  $("#profile .bio a").on("click", function(e){
+    e.preventDefault();
+    console.log("Read more clicked");
+    var $text = $("#profile .bio p");
+    var $icon = $("#profile .read-more i");
+
+    $text.toggleClass("collapse");
+
+    if ($icon.hasClass("fa-angle-up")) {
+      $icon.removeClass("fa-angle-up");
+      $icon.addClass("fa-angle-down");
+    } else {
+      $icon.removeClass("fa-angle-down");
+      $icon.addClass("fa-angle-up");
+    }
+
+    this.innerHTML = (this.innerHTML === "read more") ? "read less" : "read more";
+
+  });
 });
